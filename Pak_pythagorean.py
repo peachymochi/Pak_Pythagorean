3a954ae942119f0dba7b978ba27e6c950a44f88a
Grace Pak <gracep.41448@redlandsschools.net>
2018-09-14 15:25:25 +0000

    begining of assignment

---
+++ Pak_pythagorean.py
@@ -0,0 +1,11 @@
+"""
+Write a program that will ask the user to enter their name and respond with a greeting using this name.
+"""
+
+#Name and Greeting
+name = input("Please enter your name: ")
+print ("Hello, " + name + "!")
+
+#Length of Legs
+leg = int(input("Please enter the length of the leg 'a' of a right triangle: "))
+print (leg)
