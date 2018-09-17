e3ae48c9ed3610d6b50c6002335572a9632031ac
Grace Pak <gracep.41448@redlandsschools.net>
2018-09-17 23:31:29 +0000

    finished Pak_pythagorean.py

--- Pak_pythagorean.py
+++ Pak_pythagorean.py
@@ -7,11 +7,12 @@ name = input("Please enter your name: ")
 print ("Hello, " + name + "!")
 
 #Length of Legs
-print ("Please enter the lengths of the legs 'a' and 'b' of the right triangle: ")
+print ("Please enter the lengths of the legs 'a' and 'b' of the right triangle in order to calculate the value of 'c': ")
 
 a = float(input("a: "))
 b = float(input("b: "))
 
 
 #calculating the lenght of the hypotenuse "c"
-c = ((a**2+b**2)**(1%2)
+c = (a**2 + b**2)**(1/2)
+print ("The value of 'c' is " +str(c) + " which is also called the hypotenuse.")
